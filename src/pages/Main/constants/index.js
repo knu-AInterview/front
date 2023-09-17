3ca3c46d13f015c4e5c0fe3c5b51f7a1403b9b6c
pmhxxhsj <minho7952@naver.com>
2023-09-17 20:43:59 +0900

    feat: 드롭다운 상수 정의

---
+++ src/pages/Main/constants/index.js
@@ -0,0 +1,63 @@
+const JOB_DATAS = [
+  {
+    id: 1,
+    title: "프론트엔드 엔지니어",
+  },
+
+  {
+    id: 2,
+    title: "백엔드 엔지니어",
+  },
+  {
+    id: 3,
+    title: "데이터 엔지니어",
+  },
+];
+
+const CAREER_DATAS = [
+  {
+    id: 1,
+    title: "신입",
+  },
+  {
+    id: 2,
+    title: "1년차",
+  },
+  {
+    id: 3,
+    title: "2년차",
+  },
+  {
+    id: 4,
+    title: "3년차",
+  },
+  {
+    id: 5,
+    title: "3년차 이상",
+  },
+];
+
+const LEVEL_DATAS = [
+  {
+    id: 1,
+    title: 1,
+  },
+  {
+    id: 2,
+    title: 2,
+  },
+  {
+    id: 3,
+    title: 3,
+  },
+  {
+    id: 4,
+    title: 4,
+  },
+  {
+    id: 5,
+    title: 5,
+  },
+];
+
+export { JOB_DATAS, CAREER_DATAS, LEVEL_DATAS };
